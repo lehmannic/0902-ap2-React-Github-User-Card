54cfc962e92a27f8a5707b5dc8940098333fdedc
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-07-07 13:57:16 -0500

    🏡 BUILD: CRA + basic reset -- init commit

---
+++ github-user-cards/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Github User Cards -- Component Lifecycle Project </h1> 
+    </div>
+  );
+}
+
+export default App;
